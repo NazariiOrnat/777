81953fb818642af0284dd044ec0d9fd869a616f2
Nazar Ornat <nazarornat@mail.ru>
2016-02-25 15:12:06 +0000

    add new file

---
+++ js.js
@@ -0,0 +1,15 @@
+var a = prompt("введіть число від 1 до 3?")
+switch (a) {
+  case '1':
+    alert( 'Один' );
+
+  case '2':
+    alert( 'Два' );
+    break;
+
+  case 3:
+    alert( 'Три' );
+
+  default:
+    alert('Число завелике' + a)
+}
